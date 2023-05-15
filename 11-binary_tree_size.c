17bd64e51f4df57b98596c6e31577ae29a6b1879
Mishel450 <tomasmishel18@gmai.com>
2023-05-15 10:45:41 -0700

    binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - returns the size of the tree.
+ * @tree: it points a node of the tree.
+ * Return: 0 in case of NULL or count1 + count2.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+    {
+        return (0);
+    }
+    size_t count1 = 0, count2 = 0;
+    if (tree->left != NULL || tree->right != NULL)
+    {
+        count1 =  binary_tree_size(tree->left);
+        count2 =  binary_tree_size(tree->right);
+    }
+    return (count1 + count2 + 1);
+}
