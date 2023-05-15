f9d02b83be2030b8d53565570ed211223ab9b1c3
Mishel450 <tomasmishel18@gmai.com>
2023-05-15 14:06:14 -0700

    binary_tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,23 +6,23 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t count1 = 0;
-	size_t count2 = 0;
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	if (tree->left != NULL || tree->right != NULL)
-	{
-	count1 = 1 + binary_tree_height(tree->left);
-	count2 = 1 + binary_tree_height(tree->right);
-	}
-	if (count2 > count1)
-	{
-		return (count2);
-	}
-	else
-	{
-		return (count1);
-	}
+size_t count1 = 0;
+size_t count2 = 0;
+if (tree == NULL)
+{
+return (0);
+}
+if (tree->left != NULL || tree->right != NULL)
+{
+count1 = 1 + binary_tree_height(tree->left);
+count2 = 1 + binary_tree_height(tree->right);
+}
+if (count2 > count1)
+{
+return (count2);
+}
+else
+{
+return (count1);
+}
 }
