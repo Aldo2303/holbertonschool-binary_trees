2feff99564ab924dd440114e283e17f49ad39b37
Mishel450 <tomasmishel18@gmai.com>
2023-05-16 05:00:04 -0700

    binary_tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,14 +6,15 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t count = 0;
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    if (tree->parent != NULL)
-    {
-        count = 1 + binary_tree_depth(tree->parent);
-    }
-    return (count);
+size_t count = 0;
+
+if (tree == NULL)
+{
+return (0);
+}
+if (tree->parent != NULL)
+{
+count = 1 + binary_tree_depth(tree->parent);
+}
+return (count);
 }
