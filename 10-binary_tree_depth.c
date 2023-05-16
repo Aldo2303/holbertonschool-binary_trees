6aca545613314f2798c14f5e5048d008da857259
Mishel450 <tomasmishel18@gmai.com>
2023-05-16 05:03:05 -0700

    binary_tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_depth - return the depth of the node.
  * @tree: points to a node of the tree.
- * Return: 0 in case of NULL or count. 
+ * Return: 0 in case of NULL or count.
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
